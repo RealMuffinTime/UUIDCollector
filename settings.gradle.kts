906a6b283099ba3dd05ad9df37652548a542c8df
RealMuffinTime <realmuffintime@gmail.com>
2024-01-24 20:08:15 +0100

    update to new mc version and laby version once again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "UUIDCollector"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.30"
+    val labyGradlePluginVersion = "0.3.44"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
